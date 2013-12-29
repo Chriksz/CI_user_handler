903d16e65563b0f94f76fde0c81737265281d502
Chriksz <kredenck2@gmail.com>
2013-12-29 19:21:34 +0100

    javitva vol 2

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,22 +3,27 @@
 
 <head>
 <?php
-if (!isset($title)){
+if (!isset($title))
+{
 $title = false;
-  }
-  echo "<title>addonline.hu - $title</title>";
+}
+echo "<title>addonline.hu - $title</title>";
   ?>
   
 
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <?php if (isset($style)){
-  if (is_array ($style)){
-  foreach($style as $cssname){
-  echo "<link rel='stylesheet' type='text/css' href='/style/$cssname.css' />";
-  }
-  }
-  else{
-  echo "<link rel='stylesheet' type='text/css' href='/style/$style.css' />";
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<?php if (isset($style))
+{
+	if (is_array ($style))
+	{
+		foreach($style as $cssname)
+		{
+		echo "<link rel='stylesheet' type='text/css' href='/style/$cssname.css' />";
+		}
+	}
+  else
+  {
+	echo "<link rel='stylesheet' type='text/css' href='/style/$style.css' />";
   }
   }
   
