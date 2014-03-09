660d383df9bb4761bf2affd39754b02f40e8b106
Chriksz <Chriksz@chriksz>
2014-03-09 01:46:36 +0100

    changes, changes everywhere again

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,14 @@
-   
+<?php
+if (isset($scripts)){
+  if (is_array ($scripts)){
+  foreach($scripts as $script){
+  echo "<script src='/scripts/{$script}.js' type='text/javascript'> </script>";
+  }
+  }
+  else{
+  echo "<script src='/scripts/{$scripts}.js' type='text/javascript' ></script>";
+  }
+  }   ?>
 
 </body>
 </html>
