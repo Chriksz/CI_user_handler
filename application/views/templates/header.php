a8b3928c033bf83034e55736b0534727e6505740
Chriksz <kredenck2@gmail.com>
2014-04-15 18:33:32 +0200

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,17 +14,10 @@ echo "<title>addonline.hu - $title</title>";
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <?php if (isset($style))
 {
-	if (is_array ($style))
-	{
-		foreach($style as $cssname)
+		foreach((array) $style as $cssname)
 		{
 		echo "<link rel='stylesheet' type='text/css' href='/style/$cssname.css' />";
 		}
-	}
-  else
-  {
-	echo "<link rel='stylesheet' type='text/css' href='/style/$style.css' />";
-  }
   }
   
   ?>
